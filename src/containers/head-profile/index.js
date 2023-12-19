ee7cd5ce55aa5d4d3a4e9567c393b790f0803b8b
ildarfaz <ildarfazylbekov@gmail.com>
2023-12-19 10:38:23 +0500

    fix bug

--- src/containers/head-profile/index.js
+++ src/containers/head-profile/index.js
@@ -1,11 +1,12 @@
 
-import { memo, useCallback, useLayoutEffect, useMemo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import './style.css';
 import useStore from "../../hooks/use-store";
 import Controls from "../../components/controls";
 import { Link } from "react-router-dom";
 import useSelector from "../../hooks/use-selector";
 import { getLocalStorage } from "../../utils";
+import useInit from "../../hooks/use-init";
 
 export const HeadProfile = memo(() => {
   const store = useStore();
@@ -16,11 +17,13 @@ export const HeadProfile = memo(() => {
   const token = getLocalStorage('token');
   const callbacks = {
     onLogout: useCallback(() => store.actions.auth.logout(), [store]),
-    checkToken: useCallback(() => store.actions.auth.checkToken(), [store]),
+    checkToken: useCallback((token) => store.actions.auth.checkToken(token), [token]),
   }
-  useLayoutEffect(() => {
-    callbacks.checkToken(token);
-  }, [token]);
+  useInit(() => {
+    if (token) {
+      callbacks.checkToken(token);
+    }
+  }, [token], true);
 
   const Content = useMemo(() => {
     if (select.isAuth) {
